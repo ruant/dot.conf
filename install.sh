254d91cc64580fe880bb023b6e64c90eb125ff18
Rune Antonsen <rune.antonsen@gmail.com>
2021-04-05 19:01:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ sudo apt install docker.io
 # npm
 sudo npm install -g clipboard-cli
 
-#git
+# git
 cd /opt
 git clone https://github.com/danielmiessler/SecLists.git
 git clone https://github.com/fuzzdb-project/fuzzdb.git
+
+# copy files from this repo
+cp httpserver.py /opt/
