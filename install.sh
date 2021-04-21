ecad37659351f0bdc3dc7c6e6aa8398e85bc76ba
Rune Antonsen <rune.antonsen@gmail.com>
2021-04-21 18:05:56 +0200

    add kerbrute

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt install npm
 sudo apt install docker.io
 sudo apt install bloodhound
 sudo apt install neo4j
+sudo apt install golang-go
 
 # npm
 sudo npm install -g clipboard-cli
@@ -17,7 +18,11 @@ git clone https://github.com/ruant/SimpleHTTPServerWithUpload
 git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
 git clone https://github.com/SecureAuthCorp/impacket.git
 git clone https://github.com/CiscoCXSecurity/enum4linux.git
+git clone https://github.com/ropnop/kerbrute.git
 
 # pip it!
 pip3 install -r /opt/impacket/requirements.txt
 cd /opt/impacket/ && sudo python3 ./setup.py install
+
+# Compile and install kerbrute
+cd /opt/kerbrute/ && make all
