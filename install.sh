bfd021fd01e74486eedfb82c1f7fe2db6b2b37b7
Rune Antonsen <rune.antonsen@gmail.com>
2021-04-21 17:39:28 +0200

    run install of impacket

--- install.sh
+++ install.sh
@@ -14,3 +14,7 @@ git clone https://github.com/fuzzdb-project/fuzzdb.git
 git clone https://github.com/ruant/SimpleHTTPServerWithUpload
 git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
 git clone https://github.com/SecureAuthCorp/impacket.git
+
+# pip it!
+pip3 install -r /opt/impacket/requirements.txt
+cd /opt/impacket/ && sudo python3 ./setup.py install
